81e90f5c735283f7d64b4f39b8e904eef91d11a9
Liudmyla Liaposhchenko <liaposhchenkol@gmail.com>
2023-05-27 10:56:40 +0200

    Add component Reservation, routing

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
+import { Reservation } from '../Reservation';
 import { Home } from '../Home';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/reservation',
+    element: <Reservation />,
+  },
+]);
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <RouterProvider router={router} />
     <Footer />
   </>
 );
